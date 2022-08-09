15d6cd8f72c693062804bb91eb7eb1c78baa7c1e
Kasir <kasir.barati@gmail.com>
2022-08-09 18:33:34 +0430

    add: Greet component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
     return (
@@ -7,7 +8,9 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    <Greet />
+                    <Greet />
+                    <Greet />
                 </p>
                 <a
                     className="App-link"
---
+++ src/components/Greet.js
@@ -0,0 +1,3 @@
+export function Greet() {
+    return <h1>Hello World</h1>;
+}
